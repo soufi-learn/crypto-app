905edd63c966c683ab204cac46aabc6137f09718
Soufi learn <soufilearn@gmail.com>
2024-01-04 11:22:37 +0330

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Col, Container, Row, Stack } from "react-bootstrap";
+
+function App() {
+
+  return (
+    <>
+      <h1>Crypto App</h1>
+      <ul>
+        <li>
+          <a href="#">link 1 </a>
+        </li>
+        <li>
+          <a href="#">link 1 </a>
+        </li>
+        <li>
+          <a href="#">link 1 </a>
+        </li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, molestiae.</p>
+      <span>dddd</span>
+    </>
+  );
+}
+
+export default App
