d799284cbbc51b730274dc0f12c597967c1dc054
Soufi learn <soufilearn@gmail.com>
2024-01-11 12:13:55 +0330

    add search api to services

--- src/components/services/cryptoAPI.js
+++ src/components/services/cryptoAPI.js
@@ -1,8 +1,12 @@
 const BASE_URL = "https://api.coingecko.com/api/v3/";
 const API_KEY = "CG-oaz5qPo5aq3nJg2a4w8nzoUN";
 
-const getCoinList = () => {
-  return `${BASE_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`;
+const getCoinList = (page, currency) => {
+  return `${BASE_URL}coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`;
 };
 
-export { getCoinList };
+const searchCoin = (query) => {
+  return `${BASE_URL}search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+};
+
+export { getCoinList, searchCoin };
